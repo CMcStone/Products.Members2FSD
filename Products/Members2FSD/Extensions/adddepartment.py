f57c454d93c7e3c7e95c5e97a57e0fc82f42d121
Carol McMasters-Stone <cbeck@ucdavis.edu>
2013-11-04 16:28:05 -0800

    added files

--- Products/Members2FSD/Extensions/adddepartment.py
+++ Products/Members2FSD/Extensions/adddepartment.py
@@ -7,56 +7,34 @@ from Products.Relations.processor import process
 def AddDept():
      site = getSite()
      peeps = site['people']
-     source_contenttype = 'PersonFolder'
-     target_contenttype = 'FSDPerson'
-     directory = 
+     source_contenttype = 'FSDPerson'
+     directory = site['people']
      deptId = ""
-     items = peeps.listFolderContents(
-        contentFilter={"portal_type": target_contenttype})
+     items = director.listFolderContents(
+        contentFilter={"portal_type": source_contenttype})
         
      for item in items:
           id = item.getId()
+          if id <>"":
           classifications = item.getClassifications()
-               if classifications[0].title = 'Staff':
-                  department = 'sociology-staff'
-               elif 'faculty' in classifications[0].title:
-                  department = 'sociology-faculty' 
-               elif 'graduate' in classifications[0].title:
-                  department = 'sociology-graduate-students'
-               elif 'emeriti' in classifications[0].title:
-                  department = 'sociology-faculty'
+               if classifications[0].title = 'affiliated-faculty':
+                  department = 'mindbrain-faculty'
+               elif 'research-staff' in classifications[0].title:
+                  department = 'researchers' 
+               elif 'trainees' in classifications[0].title:
+                  department = 'researchers'
+               elif 'undergraduate-research-assistants' in classifications[0].title:
+                  department = 'researchers'
+               elif 'visiting-scholars' in classifications[0].title:
+                    department = 'mindbrain-faculty'
                else:
                     print "skipping department for" % id
           
-          if specialtyId <> "":
-            # get all the specialties assigned to this person
-            specialtiesTuple = directory[id].getSpecialties()
-            specialties = [eachSpecialtiesTuple[0].getObject().UID() for eachSpecialtiesTuple in specialtiesTuple]
-
-            # see if the specialty we are assigning is already assigned
-            if specialtyUID not in specialties:
-              print "Specialty not assigned, assigning it"
-
-              # You mustmustmust use the Relations API to add references, sayeth Relations/doc/Overview.txt.
-              process(self, connect=((directory[id].UID(), specialtyUID, specialtyRulesetId),))
-                    
-          print "Looking up the department"
-          departmentUID = self.portal_catalog(Title=department, portal_type="FSDDepartment")[0].UID
           
+                    
+         
+                 departmentUID = site.portal_catalog(Title=department, portal_type="FSDDepartment")[0].UID
+                 process(site, connect=((directory[id].UID(), departmentUID, departmentRulesetId),))
           
            
-                   
-          get sociology-faculty uid
-          assign department to member as "Sociology Faculty"
-          
-          
-          try:
-            memcontent = site.people[id].objectIds()
-            ref = site.people[id].manage_cutObjects(memcontent)
-            site._p_jar.sync()
-            site.Directory[id].manage_pasteObjects(ref)
-            transaction.commit()
-            print 'files copied'
-          except:
-            print 'no file copy'
      return "done"
