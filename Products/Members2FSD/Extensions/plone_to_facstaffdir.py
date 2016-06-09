91b5deec502ad05329b4f1bc90c9dc7cf0ed0e4d
Carol McMasters-Stone <cbeck@ucdavis.edu>
2016-06-09 16:31:29 -0700

    updating zope component hooks

--- Products/Members2FSD/Extensions/plone_to_facstaffdir.py
+++ Products/Members2FSD/Extensions/plone_to_facstaffdir.py
@@ -142,7 +142,7 @@ def reset():
 from Products.Members2FSD.interfaces import IMemberConverter
 
 from Products.CMFCore.utils import getToolByName
-from zope.app.component.hooks import getSite
+from zope.component.hooks import getSite
 import transaction
 
 class memberConverter(object):
