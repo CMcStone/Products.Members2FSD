e67adbe4f88ffe7a9754822f07aab542bff60bb7
Carol McMasters-Stone <cbeck@ucdavis.edu>
2019-08-16 14:42:16 -0700

    debug

---
+++ Products/Members2FSD/browser/exporter.py
@@ -0,0 +1,71 @@
+import csv
+import os
+from io import BytesIO
+from zope.component.hooks import getSite
+from Products.Five.browser import BrowserView
+
+from Products.CMFCore.utils import getToolByName
+
+
+class CSVExport(BrowserView):
+
+    def __call__(self):
+        buffer = BytesIO()
+        encoding = self.request.get('encoding')
+        writer = csv.writer(buffer)
+        header = [
+            'login',
+            'firstName',
+            'middleName',
+            'lastName',
+            'suffix',
+            'email',
+            'jobTitles',
+            'officeAddress',
+            'officeCity',
+            'officeState',
+            'officePostalCode',
+            'officePhone',
+            'biography',
+            'education',
+            'websites',
+
+        ]
+
+        writer.writerow(header)	
+
+        site = getSite()
+        catalog = getToolByName(site, 'portal_catalog')
+
+        people = catalog(portal_type='FSDPerson')
+
+        for person in people:
+            pobj = person.getObject()
+            row = []
+            row.append(pobj.id)
+            row.append(pobj.firstName)
+            row.append(pobj.middleName)
+            row.append(pobj.lastName)
+            row.append(pobj.suffix)
+            row.append(pobj.email)
+            row.append(pobj.jobTitles)
+            row.append(pobj.officeAddress)
+            row.append(pobj.officeCity)
+            row.append(pobj.officeState)
+            row.append(pobj.officePostalCode)
+            row.append(pobj.officePhone)
+            row.append(pobj.biography)
+            row.append(pobj.education)
+            row.append(pobj.websites)
+
+            writer.writerow(row)
+        value = buffer.getvalue()
+
+        if not encoding:
+            encoding = 'UTF-8'
+        self.request.response.setHeader('Content-type',
+                        'text/csv;charset='+encoding)
+        self.request.response.setHeader('Content-Disposition',
+                        'attachment; ilename=export.csv')
+
+        return value
