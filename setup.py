6a4a0b7d3fd0849d4b7b06ac50045e56a338a195
Carol McMasters-Stone <cbeck@ucdavis.edu>
2021-05-19 16:14:08 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,8 +6,7 @@ version = '0.1'
 setup(name='Products.Members2FSD',
       version=version,
       description="scripts for managing people",
-      long_description=open("README.rst").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
+      long_description=open("README.rst").read() + "\n" + ("HISTORY.txt").read(),
       # Get more strings from
       # http://pypi.python.org/pypi?:action=list_classifiers
       classifiers=[
